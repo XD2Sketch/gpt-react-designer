78472450fe835562a3d0e69aefa172d34a65bbb2
Niels Bosman <nielsbosman@mail.com>
2024-02-04 12:28:47 +0100

    feat: use cheap gpt-3 models

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -12,7 +12,7 @@ export const runtime = 'edge'
 export async function POST(req: NextRequest) {
   const { messages } = await req.json()
   const response = await openai.createChatCompletion({
-    model: 'gpt-4-1106-preview',
+    model: 'gpt-3.5-turbo',
     stream: true,
     messages
   })
